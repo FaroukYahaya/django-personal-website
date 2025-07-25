c2b0245a49ffc0cac6d0ebce658785f1445fefdf
Farouk Yahaya <faroukyahayaco@gmail.com>
2025-07-25 17:33:30 +0200

    Update build.sh
    removed useruser creation line

--- build.sh
+++ build.sh
@@ -10,17 +10,6 @@ python manage.py makemigrations --noinput
 echo "Applying migrations..."
 python manage.py migrate --noinput
 
-echo "Creating superuser if it doesn't exist..."
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('farouk', 'faroya2011@gmail.com', 'Fadams233')
-    print('Superuser created: farouk/Fadams233')
-else:
-    print('Superuser already exists')
-"
-
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
